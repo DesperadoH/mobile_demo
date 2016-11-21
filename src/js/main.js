3fc1eeb939dc03c79c50150699e7625d3a07942b
DesperadoH <youxiyibai@126.com>
2016-11-21 17:37:58 +0800

    1rem=75px

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+	(function(){
+
+		var num = 1 / window.devicePixelRatio;
+		document.write('<meta name="viewport" content="width=device-width,initial-scale='+num+',minimum-scale='+num+',maximum-scale='+num+',user-scalable=no" />');
+
+		var fz = document.documentElement.clientWidth / 10;
+		document.getElementsByTagName("html")[0].style.fontSize = fz + "px";
+
+	})()
+
